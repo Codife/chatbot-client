bb0e505c86405ae6e318797be018b97689608aa1
hunainjamil <hunainjamil3@gmail.com>
2022-12-09 12:42:43 +0500

    updated ui

--- src/components/ChatContainer/index.js
+++ src/components/ChatContainer/index.js
@@ -1,8 +1,16 @@
+import classNames from "classnames";
 import React from "react";
 import styles from "./ChatContainer.module.css";
 
 const ChatContainer = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.topContainer}>
+        <h3 className={styles.text}>EXII Bot</h3>
+        <p className={classNames(styles.text, styles.subText)}>2nd Sep 2022</p>
+      </div>
+    </div>
+  );
 };
 
 export default ChatContainer;
