c233070b4d3e303e73489e565c5fed58e5a3b9a8
Codife <57570657+Codife@users.noreply.github.com>
2022-12-12 13:53:52 +0500

    updated ui

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,9 +2,16 @@ import classNames from "classnames";
 import React from "react";
 import styles from "./Button.module.css";
 
-const Button = ({ text, onClick, isSelected }) => {
+const Button = ({ text, onClick, isSelected, customStyle }) => {
   return (
-    <div className={classNames(styles.container, isSelected && styles.selected)} onClick={onClick}>
+    <div
+      className={classNames(
+        styles.container,
+        isSelected && styles.selected,
+        customStyle
+      )}
+      onClick={onClick}
+    >
       {text}
     </div>
   );
--- src/components/Topbar/index.js
+++ src/components/Topbar/index.js
@@ -6,11 +6,15 @@ import styles from "./Topbar.module.css";
 const Topbar = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.subContainer}>
+      <div className={styles.btnContainer}>
         <Button text="About" onClick={() => null} isSelected />
         <Button text="Discord" onClick={() => null} />
         <Button text="Video Chat" onClick={() => null} />
+      </div>
+      <div className={styles.exiContainer}>
         <img src={EXI} className={styles.exiImg} />
+      </div>
+      <div className={styles.btnContainer}>
         <Button text="Lands" onClick={() => null} />
         <Button text="Readings" onClick={() => null} />
         <Button text="On This Day" onClick={() => null} />
